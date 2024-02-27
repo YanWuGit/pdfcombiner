b5fd776cca933d32f14a56b0d47db7614d29282a
Beluga_samsung <2x3tech@gmail.com>
2024-02-26 17:56:10 -0800

    initial

---
+++ src/main/java/combiner/PdfcombinerApplication.java
@@ -0,0 +1,13 @@
+package combiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfcombinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfcombinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/combiner/PdfcombinerApplicationTests.java
@@ -0,0 +1,13 @@
+package combiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfcombinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
